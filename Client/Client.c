310c82f9fc21015673bb532410f5d9906ea8bc52
YongBhin <coji67@gmail.com>
2022-07-20 17:07:07 +0900

    JNI를 이용한 암/복호화 통신 [Client]

--- Client/Client.c
+++ Client/Client.c
@@ -1,8 +1,14 @@
 #include <jni.h>
 #include "Client.h"
 #include <stdio.h>
-// #include "../blockCipher/blockCipherLib.h"
+// ========================================================================
+// 블록암호 헤더 참조
+// ========================================================================
+
+// [SimpleCipher]
 // #include "../blockCipher/simpleCipherLib.h"
+
+// [AES]
 #include "../blockCipher/aesLib.h"
 
 JNIEXPORT jintArray JNICALL Java_Client_Enc(JNIEnv *env, jobject jobj, jintArray arr2)
@@ -11,20 +17,14 @@ JNIEXPORT jintArray JNICALL Java_Client_Enc(JNIEnv *env, jobject jobj, jintArray
     int size, i;
     jintArray out;
     unsigned char key[] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x16, 0x47, 0x77, 0x11, 0x1e, 0x3c};
-    // unsigned char EncBuff[80];
-    unsigned char EncBuff[128];
-    // unsigned char DecBuff[80];
-    // int intkey[10];
-    // for (int i = 0; i < 10; i++) {
-        // intkey[i] = key[i];
-    // }
+    unsigned char EncBuff[16];
     
-    out = (*env)->NewIntArray(env, 128);
+    out = (*env)->NewIntArray(env, 16);
     size = (*env)->GetArrayLength(env, arr2);
     byteBuf = (*env)->GetIntArrayElements(env, arr2, 0);
-    unsigned char tmp[128];
-    int tmp2[128];
-    for (int i = 0; i < 128; i++) {
+    unsigned char tmp[16];
+    int tmp2[16];
+    for (int i = 0; i < 16; i++) {
         if (byteBuf[i] > 255) {
             tmp[i] = 0;
         }
@@ -36,22 +36,16 @@ JNIEXPORT jintArray JNICALL Java_Client_Enc(JNIEnv *env, jobject jobj, jintArray
         }
     }
     printf("[입력 메시지(hex)] \n");
-    // printState(tmp);
     printState_AES(tmp);
 
-    // Enc(tmp, intkey);
-    AES_ECB_Encrypt(tmp, key, EncBuff, 128);
+    AES_ECB_Encrypt(tmp, key, EncBuff, 16);
     printf("[암호화(hex)] \n");
-    // printState(tmp);
     printState_AES(EncBuff);
-    // for (int i = 0; i < 128; i++) {
-    //     printf("%d", byteBuf[i]);
-    // }
-    for (int i=0; i<128; i++) {
+    for (int i=0; i<16; i++) {
         tmp2[i] = (int)EncBuff[i];
     }
 
-    (*env)->SetIntArrayRegion(env, out, 0, 128, tmp2);
+    (*env)->SetIntArrayRegion(env, out, 0, 16, tmp2);
     (*env)->ReleaseIntArrayElements(env, arr2, byteBuf, 0);
 
     return out;
@@ -63,35 +57,26 @@ JNIEXPORT jintArray JNICALL Java_Client_Dec(JNIEnv *env, jobject jobj, jintArray
     jint *byteBuf;
     int size, i;
     jintArray out;
-    // unsigned char DecBuff[80];
-    unsigned char DecBuff[128];
-    // char key[] = "mykeymykey";
+    unsigned char DecBuff[16];
     unsigned char key[] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x16, 0x47, 0x77, 0x11, 0x1e, 0x3c};
-    // int intkey[10];
-    // for (int i = 0; i < 10; i++) {
-    //     intkey[i] = key[i];
-    // }
-    out = (*env)->NewIntArray(env, 128);
+    out = (*env)->NewIntArray(env, 16);
     size = (*env)->GetArrayLength(env, arr2);
     byteBuf = (*env)->GetIntArrayElements(env, arr2, 0);
-    // int tmp[128];
-    unsigned char tmp[128];
-    int tmp2[128];
-    // printf("[받은 메시지 : C ]\n");
-    for (int i = 0; i < 128; i++) {
+    unsigned char tmp[16];
+    int tmp2[16];
+    for (int i = 0; i < 16; i++) {
         tmp[i] = byteBuf[i];
-        // printf("%d ", tmp[i]);
     }
-    // Dec(tmp, intkey);
-    AES_ECB_Decrypt(tmp, key, DecBuff, 128);
+
+    AES_ECB_Decrypt(tmp, key, DecBuff, 16);
+
     printf("[복호화(hex)]\n");
-    // printState(tmp);
     printState_AES(DecBuff);
-    for (int i=0; i<128; i++) {
+    for (int i=0; i<16; i++) {
         tmp2[i] = (int)DecBuff[i];
     }
 
-    (*env)->SetIntArrayRegion(env, out, 0, 128, tmp2);
+    (*env)->SetIntArrayRegion(env, out, 0, 16, tmp2);
     (*env)->ReleaseIntArrayElements(env, arr2, byteBuf, 0);
 
     return out;
--- Client/Client.java
+++ Client/Client.java
@@ -2,13 +2,13 @@ import java.io.*;
 import java.net.*; 
 class Client { 
     public static int[] stringToInt(String s) {
-        int[] result = new int[128];
+        int[] result = new int[16]; // 128
         
     
         for (int i = 0; i < s.length(); i++) {
             result[i] = (int) s.charAt(i);
         }
-        for (int i = s.length(); i < 128; i++){
+        for (int i = s.length(); i < 16; i++){// 128
             result[i] = 0;
         }
     
@@ -18,41 +18,63 @@ class Client {
     public native int[] Dec(int[] tmp);
     public static void main(String argv[]) throws Exception { 
 
-        Socket clientSocket = new Socket("localhost", 10000); // Client 포트 : 10000
+        // Client는 10000 포트(Server)로 연결 요청
+        Socket clientSocket = new Socket("localhost", 10000);
+
+        // Client/Server 연결 완료
         System.out.println("[client-server connected]\n");
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); // 입력값 받기
+
+        // Client는 사용자로부터 메시지를 입력받음
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
         int[] PT = stringToInt(inFromUser.readLine());
         Client h = new Client();
-        int[] out = h.Enc(PT);                              // int 배열 JNI 이용하여 C로 넘겨 암호화
+
+        // 입력받은 메시지를 int 배열로 변경하고 JNI를 이용하여 블록암호 AES로 암호화
+        int[] out = h.Enc(PT);
+
+
+        // 암호화된 배열을 String으로 변경
         StringBuilder inttoSB = new StringBuilder();
-        for (int i = 0; i < 128; i++) {
-            inttoSB.append(String.valueOf(out[i]));         // "val val val val"
-            if (i < 127)
+        for (int i = 0; i < 16; i++) {
+            inttoSB.append(String.valueOf(out[i]));
+            if (i < 15)
                 inttoSB.append(" ");
         }
-        String clientSentence = inttoSB.toString();         // "val val val val"
+        String clientSentence = inttoSB.toString();
 
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); 
-        outToServer.writeBytes(clientSentence + "\n");      // send encrypted input string to server
+
+        // 암호화된 메시지를 Server로 전송
+        outToServer.writeBytes(clientSentence + "\n");      
 
         System.out.println("[Client] : Sending an encrypted message to server.\n");
+
+        // Server로부터 암호화된 메시지를 받음
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String receivedSentence = inFromServer.readLine();  // received encrypted string sentence from server
-        String[] CT = receivedSentence.split(" ");   // ["val"] ["val"] ["val"] ["val"]
+        String receivedSentence = inFromServer.readLine();
+
+        String[] CT = receivedSentence.split(" ");   
         int[] CT2 = new int[CT.length];                     
         for (int i = 0; i < CT.length; i++) {
-            CT2[i] = Integer.parseInt(CT[i]);               // [val] [val] [val] [val]
+            CT2[i] = Integer.parseInt(CT[i]);  
         }
 
 
+        // Server로부터 받은 메시지를 블록암호 AES를 이용하여 복호화
         Client j = new Client();
-        int[] out2 = j.Dec(CT2);                            // [val] [val] [val] [val]
+        int[] out2 = j.Dec(CT2);
+
         StringBuilder inttoStr = new StringBuilder();
         for (int i = 0; i < PT.length; i++) {
             inttoStr.append((char) out2[i]);                // [char] [char] [char] [char]
         }
-        String tmp = inttoStr.toString();                   // "char char char char" 
+        String tmp = inttoStr.toString();
+
+        // Client는 복호화된 메시지를 출력
         System.out.println("[Client] : Decrypted Server message. \n" + tmp);
+
+        // 소켓 닫기
         clientSocket.close(); 
     } 
 
